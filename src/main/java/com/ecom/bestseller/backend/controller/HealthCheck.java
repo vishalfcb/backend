fdc244e879222af64d274cd8416e81f546e7f0df
vishalfcb <vishal.dey32@gmail.com>
2025-05-15 00:36:38 +0530

    init commit

--- src/main/java/com/ecom/bestseller/backend/controller/HealthCheck.java
+++ src/main/java/com/ecom/bestseller/backend/controller/HealthCheck.java
@@ -1,4 +1,15 @@
 package com.ecom.bestseller.backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/healthcheck")
 public class HealthCheck {
+    @GetMapping
+    public String isFind(){
+        return "Hi there";
+    }
+
 }
